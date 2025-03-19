430a8eda852660ad3a037559bf0fa12766fea341
scilentgemini <rbchitthi@gmail.com>
2025-03-20 00:53:44 +0545

    dashboard filters applied

--- app/Filament/Widgets/TestChartWidget.php
+++ app/Filament/Widgets/TestChartWidget.php
@@ -4,21 +4,29 @@ namespace App\Filament\Widgets;
 
 use App\Models\User;
 use Flowframe\Trend\Trend;
+use Illuminate\Support\Carbon;
 use Flowframe\Trend\TrendValue;
 use Filament\Widgets\ChartWidget;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
 
 class TestChartWidget extends ChartWidget
 {
+
+    use InteractsWithPageFilters;
+
     protected static ?string $heading = 'Chart';
     protected int | string | array $columnSpan = '1';
 
     protected function getData(): array
     {
 
+        $start = $this->filters['startDate'];
+        $end = $this->filters['endDate'];
+
         $data = Trend::model(User::class)
         ->between(
-            start: now()->subMonths(6),
-            end: now(),
+            start: $start ? Carbon::parse($start) : now()->subMonths(6),
+            end: $end ? Carbon::parse($end) : now(),
         )
         ->perMonth()
         ->count();
--- app/Filament/Widgets/TestWidget.php
+++ app/Filament/Widgets/TestWidget.php
@@ -5,20 +5,30 @@ namespace App\Filament\Widgets;
 use App\Models\User;
 use Filament\Support\Enums\IconPosition;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 
 class TestWidget extends BaseWidget
 {
+
+    use InteractsWithPageFilters;
+
+
     protected function getStats(): array
     {
-        return [
-            Stat::make('New Users', User::count())
-            ->description('Newly Joined Users')
-            ->descriptionIcon('heroicon-m-user-group', IconPosition::Before)
-            ->chart([10,20,40, 30, 50, 70, 80])
-            ->color('success'),
 
-            Stat::make('New Users', User::count())
+        $start = $this->filters['startDate'];
+        $end = $this->filters['endDate'];
+
+        return [
+            Stat::make('New Users',
+            User::
+            when($start,
+            fn ($query) => $query->whereDate('created_at', '>',$start) )
+            ->when($end,
+            fn ($query) => $query->whereDate('created_at', '<',$end) )
+
+            ->count())
             ->description('Newly Joined Users')
             ->descriptionIcon('heroicon-m-user-group', IconPosition::Before)
             ->chart([10,20,40, 30, 50, 70, 80])
